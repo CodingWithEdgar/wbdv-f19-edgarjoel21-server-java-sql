b1c44dc833b5d7297715b77a745cb82ab472f221
edgarjoel21 <maldonado.ed@husky.neu.edu>
2019-11-15 19:07:31 -0500

    Allow cross origin

--- main/java/com/example/persistingdatareactJPA/services/FacultyService.java
+++ main/java/com/example/persistingdatareactJPA/services/FacultyService.java
@@ -3,6 +3,7 @@ package com.example.persistingdatareactJPA.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +14,7 @@ import com.example.persistingdatareactJPA.models.Faculty;
 import com.example.persistingdatareactJPA.repositories.CourseRepository;
 import com.example.persistingdatareactJPA.repositories.FacultyRepository;
 
+@CrossOrigin(origins="*",allowCredentials="true",allowedHeaders="*")
 @RestController
 public class FacultyService {
 	@Autowired
